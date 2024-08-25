bb2799f18c36531050b64822c5ba318ca9ca83a5
nedhle <a.sahin28@gmail.com>
2024-08-25 20:30:25 +0200

    Add exceptions

---
+++ src/main/java/app/exception/DeviceNotFoundException.java
@@ -0,0 +1,8 @@
+package app.exception;
+
+public class DeviceNotFoundException extends RuntimeException {
+
+    public DeviceNotFoundException(Long id) {
+        super("Device with ID " + id + " not found");
+    }
+}
