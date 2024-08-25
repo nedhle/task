e044ebc52205865d2801fc55224284bf8c7a3f5e
nedhle <a.sahin28@gmail.com>
2024-08-26 00:01:56 +0200

    Add operations name

--- src/main/java/app/controller/DeviceController.java
+++ src/main/java/app/controller/DeviceController.java
@@ -3,6 +3,8 @@ package app.controller;
 import app.controller.request.DeviceRequest;
 import app.model.Device;
 import app.service.DeviceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +14,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/devices")
 @RequiredArgsConstructor
+@Tag(name = "Device Controller", description = "API for managing devices")
 public class DeviceController {
 
     private final DeviceService deviceService;
 
+    @Operation(summary = "Add a new device")
     @PostMapping
     public ResponseEntity<Device> addDevice(@RequestBody DeviceRequest deviceRequest) {
         return ResponseEntity.ok(deviceService.addDevice(deviceRequest));
     }
 
+    @Operation(summary = "Get a list of all devices")
     @GetMapping
     public ResponseEntity<List<Device>> listAllDevices() {
         return ResponseEntity.ok(deviceService.listAllDevices());
     }
 
+    @Operation(summary = "Get a device by ID")
     @GetMapping("/{id}")
     public ResponseEntity<Device> getDeviceById(@PathVariable Long id) {
         return deviceService.getDeviceById(id)
@@ -33,22 +39,26 @@ public class DeviceController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Update a device by ID")
     @PutMapping("/{id}")
     public ResponseEntity<Device> updateDevice(@PathVariable Long id, @RequestBody DeviceRequest deviceRequest) {
         return ResponseEntity.ok(deviceService.updateDevice(id, deviceRequest));
     }
 
+    @Operation(summary = "Partially update a device by ID")
     @PatchMapping("/{id}")
     public ResponseEntity<Device> partialUpdateDevice(@PathVariable Long id, @RequestBody DeviceRequest deviceRequest) {
         return ResponseEntity.ok(deviceService.patchDevice(id, deviceRequest));
     }
 
+    @Operation(summary = "Delete a device by ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
         deviceService.deleteDevice(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Search devices by brand")
     @GetMapping("/by-brand")
     public ResponseEntity<List<Device>> searchDevicesByBrand(@RequestParam String brand) {
         return ResponseEntity.ok(deviceService.searchDevicesByBrand(brand));
