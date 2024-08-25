0ef212071884bf2d72aada6d9f614c8734940eae
nedhle <a.sahin28@gmail.com>
2024-08-25 20:26:32 +0200

    Add device mapper

---
+++ src/main/java/app/mapper/DeviceMapper.java
@@ -0,0 +1,18 @@
+package app.mapper;
+
+import app.controller.request.DeviceRequest;
+import app.dto.DeviceDto;
+import app.model.Device;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface DeviceMapper {
+
+    DeviceMapper INSTANCE = Mappers.getMapper(DeviceMapper.class);
+
+    Device dtoToEntity(DeviceDto deviceDto);
+
+    DeviceDto requestToDto(DeviceRequest deviceRequest);
+
+}
